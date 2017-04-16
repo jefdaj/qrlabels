a4feeb6bd5d98f738bb5763d6438ca9bc217e017
Jeffrey David Johnson <jefdaj@gmail.com>
2017-04-15 23:41:39 -0700

    refactor functions

--- qrlabels/qrlabels.py
+++ qrlabels/qrlabels.py
@@ -30,57 +30,64 @@ Options:
      <pdfpath>  Where to write the QR code images
 '''
 
-from reportlab.lib.colors       import lightgrey
-from docopt                     import docopt
-from math                       import sqrt
-from reportlab.graphics.barcode import qr
-from reportlab.graphics.shapes  import Drawing 
-from reportlab.lib.pagesizes    import letter, inch
-from reportlab.lib.styles       import getSampleStyleSheet
-from reportlab.platypus         import SimpleDocTemplate, TableStyle, Paragraph
-from reportlab.platypus.tables  import Table
-from shortuuid                  import uuid
-from sys import argv
+from docopt                        import docopt
+from reportlab.graphics.barcode.qr import QrCodeWidget
+from reportlab.graphics.shapes     import Drawing
+from reportlab.lib.colors          import lightgrey
+from reportlab.lib.pagesizes       import letter, inch
+from reportlab.lib.styles          import getSampleStyleSheet
+from reportlab.platypus            import SimpleDocTemplate, TableStyle, Paragraph
+from reportlab.platypus.tables     import Table
+from shortuuid                     import uuid
+from sys                           import argv
 
-def generate_qrcode(prefix, nchar):
-  return qr.QrCodeWidget(prefix + uuid()[:nchar])
+def qrcode(prefix, nchar):
+  return QrCodeWidget(prefix + uuid()[:nchar])
 
-def fix_page_layout(canv, doc):
-  frame = doc.pageTemplates[0].frames[0]
-  frame.leftPadding=frame.rightPadding=frame.topPadding=frame.bottomPadding=0
-  canv.saveState()
-
-def save_pdf(prefix, nchar, ncol, nrow, filename):
-  s = getSampleStyleSheet()['Normal']
-  wm = Paragraph(' '.join(['qrlabels']+argv[1:]), s)
-  doc = SimpleDocTemplate(filename, pagesize=letter,
-                          rightMargin=0.45*inch, leftMargin=0.45*inch,
-                          topMargin=0.45*inch-s.leading, bottomMargin=0.45*inch)
-  qw = doc.width  / ncol
-  qh = (doc.height - s.leading) / nrow
-  rws = [qw] * ncol
-  rhs = [qh] * nrow
-  ls = 0.18 * inch
+def qrcodes(prefix, nchar, nrow, ncol, qrsize):
   rows = []
   for r in range(1, nrow+1):
     row = []
     for c in range(1, ncol+1):
-      qrcode = generate_qrcode(prefix, nchar)
-      bounds = qrcode.getBounds()
-      width  = bounds[2]
-      height = bounds[3]
-      label  = Drawing(ls, ls, transform=[ls/width, 0, 0, ls/height, 0, 0])
-      label.add(qrcode)
+      qrc = qrcode(prefix, nchar)
+      bounds = qrc.getBounds()
+      label  = Drawing(qrsize, qrsize, transform=[qrsize/bounds[2], 0, 0,
+                                                  qrsize/bounds[3], 0, 0])
+      label.add(qrc)
       row.append(label)
     rows.append(row)
-  t = Table(rows, colWidths=rws, rowHeights=rhs, hAlign='CENTER', vAlign='MIDDLE')
-  t.setStyle(TableStyle([ ('ALIGN'    , (0,0), (-1,-1), 'CENTER'),
-                          ('VALIGN'   , (0,0), (-1,-1), 'MIDDLE'),
-                          ('INNERGRID', (0,0), (-1,-1), 1.0, lightgrey),
-                          ('BOX'      , (0,0), (-1,-1), 1.0, lightgrey) ]))
-  return doc.build([wm, t], onFirstPage=fix_page_layout, onLaterPages=fix_page_layout)
+  return rows
+
+def table(doc, style, prefix, nchar, nrow, ncol, qrsize):
+  label_widths  = [doc.width  / ncol] * ncol
+  label_heights = [(doc.height - style.leading) / nrow] * nrow
+  data = qrcodes(prefix, nchar, nrow, ncol, qrsize)
+  tbl = Table(data, colWidths=label_widths, rowHeights=label_heights,
+              hAlign='CENTER', vAlign='MIDDLE')
+  tbl.setStyle(TableStyle([ ('ALIGN'    , (0,0), (-1,-1), 'CENTER'),
+                            ('VALIGN'   , (0,0), (-1,-1), 'MIDDLE'),
+                            ('INNERGRID', (0,0), (-1,-1), 1.0, lightgrey),
+                            ('BOX'      , (0,0), (-1,-1), 1.0, lightgrey) ]))
+  return tbl
+
+def kludge(canvas, doc):
+  frame = doc.pageTemplates[0].frames[0]
+  frame.leftPadding=frame.rightPadding=frame.topPadding=frame.bottomPadding=0
+  canvas.saveState()
+
+def pdf(prefix, nchar, ncol, nrow, filename):
+  style = getSampleStyleSheet()['Normal']
+  wm = Paragraph(' '.join(['qrlabels']+argv[1:]), style)
+  doc = SimpleDocTemplate(filename, pagesize=letter,
+                          rightMargin  = 0.45*inch,
+                          leftMargin   = 0.45*inch,
+                          topMargin    = 0.45*inch-style.leading,
+                          bottomMargin = 0.45*inch)
+  qrsize = 0.18 * inch
+  t = table(doc, style, prefix, nchar, nrow, ncol, qrsize)
+  return doc.build([wm, t], onFirstPage=kludge, onLaterPages=kludge)
 
 def main():
   args = docopt(__doc__, version='qrlabels 0.1')
   global verbose; verbose = args['-v']
-  save_pdf(args['-p'], 7, 12, 16, args['<pdfpath>'])
+  pdf(args['-p'], 7, 12, 16, args['<pdfpath>'])
